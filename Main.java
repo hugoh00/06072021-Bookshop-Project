e9aa58bce6ceaddd7d833aef8928a493c166160e
Hugo Hay <hugohay00@outlook.com>
2021-07-06 13:03:11 +0100

    V0.1 - Stock Added (Get/Sets)

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
-/* 
-Bookshop Project
-*/
+public class Main {
+    /**
+	 * The main entry point of the program
+	 * @param args - not used
+	 */
+	public static void main(String[] args) {}
+}
---
+++ Stock.java
@@ -0,0 +1,65 @@
+/**
+ * @author H. Hay
+ */
+public class Stock {
+
+    private int stockID;
+    private String stockName;
+    private int stockPrice;
+
+    public Stock(int stockID, String stockName, int stockPrice) {
+        this.setStockID(stockID);
+        this.setStockName(stockName);
+        this.setStockPrice(stockPrice);
+
+    } 
+
+    /**
+	 * Sets the ID of the stock that this item of stock is about
+	 * @param stockID the ID of the stock
+	 */
+    private void setStockID(int stockID) {
+        this.stockID = stockID;
+    }
+
+    /**
+	 * Sets the Name of the stock that this item of stock is about
+	 * @param stockName the Name of the stock
+	 */
+    private void setStockName(int stockName) {
+        this.stockName = stockName;
+    }
+
+    /**
+	 * Sets the Price of the stock that this item of stock is about
+	 * @param stockPrice the Price of the stock
+	 */
+    private void setStockPrice(int stockPrice) {
+        this.stockPrice = stockPrice;
+    }
+
+    /**
+	 * Returns the ID of the stock item
+	 * @return the Name of the Stock
+	 */
+	public int getStockID() {
+		return stockID;
+	}
+
+    /**
+	 * Returns the Name of the stock item
+	 * @return the Name of the Stock
+	 */
+	public String getStockName() {
+		return stockName;
+	}
+    
+    /**
+	 * Returns the Price of the stock item
+	 * @return the Price of the Stock
+	 */
+	public int getStockPrice() {
+		return stockPrice;
+	}
+
+}
